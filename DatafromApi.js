5eef06ba6e76dbd1164978fb0bf9c2689e99a79a
Aryan yadav <107910961+aryan-ya@users.noreply.github.com>
2023-06-02 08:16:15 +0530

    Create DatafromApi.js

---
+++ DatafromApi.js
@@ -0,0 +1,16 @@
+// 8. Get Data from API and Display it on the browser console.
+// Write a JavaScript program that uses the Fetch method to retrieve data from an API, and then logs the data to
+// the console. For example, you could use the API at https://jsonplaceholder.typicode.com/posts to retrieve a list
+// of posts, and then display them to the browser console.
+
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+  })
+  .catch(function(error) {
+    console.log('Error:', error);
+  });
